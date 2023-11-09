7c4d2b3f961006872ffab03211f1717b24ba8047
Saeed Paracha <saeedparacha13@gmail.com>
2023-11-09 22:51:06 +0500

    testing

--- resources/js/field/module/Array/index.vue
+++ resources/js/field/module/Array/index.vue
@@ -6,30 +6,50 @@
             item-key="id"
             v-model="array"
             @end="changeArray(array)"
+            :disabled="!isForm"
         >
             <template #item="{ element }">
-                <div class="relative w-32">
-                    <div class="title truncate text-center" v-text="element.title || element.name" />
-                    <img class="h-40 w-32" :src="element.url" />
-                    <!-- <button v-if="isForm" @click="remove(i)">Remove</button> -->
-                </div>
+                <!-- <div class="nml-item"> -->
+                    <!-- <div class="relative w-32">
+                        <div class="title truncate text-center" v-text="element.title || element.name" />
+                        <img class="h-40 w-32" :src="element.url" />
+                        // <button v-if="isForm" @click="remove(i)">Remove</button>
+                    </div> -->
+                    <a :href="element.url" target="_blank" class="no-underline">
+                        <img class="block rounded-lg shadow-md max-w-xs"
+                            v-if="`image` === mime(element)"
+                            :src="element.preview || element.url"
+                            :alt="__('This file could not be found')" />
+
+                        <div v-else>
+                            <div class="nml-item relative mb-2 cursor-pointer" :title="element.title || element.name">
+
+                            <div :class="'icon rounded-lg shadow-md nml-icon-'+mime(element)" :style="bg(element)" />
+
+                            <div class="title truncate" v-text="element.title || element.name" />
+                            <svg v-if="isForm" class="delete shadow-md dim" @click.stop="remove(i)"><icon-delete/></svg>
+                            </div>
+                        </div>
+                    </a>
             </template>
+
+
         </draggable>
 
         <div
-            class="card border-lg border-50 max-w-xs max-w-xs cursor-pointer border p-8 text-center"
+            class="card border-lg border-50 max-w-xs cursor-pointer border p-8 text-center"
             v-else-if="isForm"
             @click="popup = true"
         >
             {{ __('Select Files') }}
         </div>
 
-        <div class="mt-4" v-if="isForm && array && array.length">
-            <a class="dim text-primary inline-block cursor-pointer font-bold" @click="popup = true">
+        <div class="mt-4 space-x-5" v-if="isForm && array && array.length">
+            <a class="dim text-primary-500 inline-block cursor-pointer font-bold" @click="popup = true">
                 {{ __('Media Library') }}
             </a>
 
-            <a class="dim text-danger ml-8 inline-block cursor-pointer font-bold" @click="changeArray([])">
+            <a class="dim text-red-500 ml-8 inline-block cursor-pointer font-bold" @click="changeArray([])">
                 {{ __('Clear') }}
             </a>
         </div>
